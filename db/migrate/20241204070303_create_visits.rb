2fc6d2e29dfb9fa6657c461c7da96370b4dd2c03
Chris <ooichristopher8@gmail.com>
2024-12-04 12:20:40 -0500

    Updated db migration for Visits

--- db/migrate/20241204070303_create_visits.rb
+++ db/migrate/20241204070303_create_visits.rb
@@ -1,6 +1,13 @@
 class CreateVisits < ActiveRecord::Migration[8.0]
   def change
     create_table :visits do |t|
+      t.references :link, null: false, foreign_key: true
+      t.string :ip_address
+      t.string :user_agent
+      t.string :referer
+      t.string :country
+      t.string :city
+      
       t.timestamps
     end
   end

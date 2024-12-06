d9b2ef849eda00e226eeda9240ce84185619b337
Chris <ooichristopher8@gmail.com>
2024-12-06 18:51:58 -0500

    .

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,2 +1,21 @@
 class Visit < ApplicationRecord
+    belongs_to :link
+
+    validates :ip_address, presence: true   # @attr [String] ip_address The IP address of the visitor
+    validates :user_agent, presence: true   # @attr [String] user_agent The user agent string of the visitor's browser
+
+    before_create :set_geolocation
+
+    private
+
+    def set_geolocation
+        result = Geocoder.search(ip_address).first
+        if result
+            self.country = result.country
+            self.city = result.city
+        end
+    rescue StandardError => e
+        Rails.logger.error("Error setting geolocation: #{e.message}")
+    end
+    
 end

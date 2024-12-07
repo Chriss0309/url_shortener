e0b0c07d7ec83e982c1ea288c4a627dc400fc8b7
Chris <ooichristopher8@gmail.com>
2024-12-07 03:33:29 -0500

    .

--- config/initializers/turnstile.rb
+++ config/initializers/turnstile.rb
@@ -1,4 +1,6 @@
 require 'turnstile'
 
-ENV['TURNSTILE_SITE_KEY'] = '0x4AAAAAAA1qYF0UJJJz-SR9'
-ENV['TURNSTILE_SECRET_KEY'] = '0x4AAAAAAA1qYNDvnhEylFz4SwwRnvBRGTc' 
+# Don't override environment variables if they're already set
+ENV['TURNSTILE_SITE_KEY'] ||= '0x4AAAAAAA1qYF0UJJJz-SR9'     # Development fallback
+ENV['TURNSTILE_SECRET_KEY'] ||= '0x4AAAAAAA1qYNDvnhEylFz4SwwRnvBRGTc'  # Development fallback
+  
